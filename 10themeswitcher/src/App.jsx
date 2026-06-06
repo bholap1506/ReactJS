ef79194ce6e8b1bb5443aaca8af46b8a6aafa332
bholap1506 <avinashrajpatel1506@gmail.com>
2026-06-06 23:24:59 +0530

    code change in App.jsx

--- 10themeswitcher/src/App.jsx
+++ 10themeswitcher/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
   return (    
     <ThemeProvider value={{themeMode, lightMode, darkMode}}>
-    <div className="flex flex-wrap min-h-screen items-center"> 
+    <div className="flex flex-wrap min-h-screen items-center bg-black"> 
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
                       {/* themesBtn  */}
